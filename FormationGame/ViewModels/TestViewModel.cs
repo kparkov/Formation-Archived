0e617f5ee274272951d26a4e05d138b7e2439700
Kristian Videmark Parkov <kparkov@gmail.com>
2014-03-14 00:44:06 +0100

    Test configuration

---
+++ FormationGame/ViewModels/TestViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FormationGame.ViewModels
+{
+    public class TestViewModel
+    {
+        // Properties
+        public string MessageForTheUser { get; set; }
+
+        // Methods
+        public string GetACountFromTo(int from, int to)
+        {
+            List<int> ListOfNumbers = new List<int>();
+
+            for (int i = from; i <= to; i++)
+            {
+                ListOfNumbers.Add(i);
+            }
+
+            return String.Join(", ", ListOfNumbers);
+        }
+    }
+}
