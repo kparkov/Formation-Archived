22c5a4270f7351db76f8e9bbbb3f3c844cb71b90
esbenheick <esbenheick@gmail.com>
2014-03-18 22:11:47 +0100

    esben har her skrevet en commit-message

---
+++ FormationGame/Controllers/EsbentestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormationGame.Controllers
+{
+    public class EsbentestController : Controller
+    {
+        //
+        // GET: /Esbentest/
+
+        public ActionResult MyNewAction(){
+            return View();
+        }
+
+    }
+}
