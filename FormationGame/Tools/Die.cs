fb750716acecfcac37eea0c8a427a27086b56d03
Kristian Videmark Parkov <kparkov@gmail.com>
2014-03-24 22:56:09 +0100

    Added comments to dice code

--- FormationGame/Tools/Die.cs
+++ FormationGame/Tools/Die.cs
@@ -10,6 +10,10 @@
         public string Color { get; set; }
 
 		// Metoder / Funktioner: Hvad kan en terning? Hvilke handlinger er mulige med en terning?
+
+        /**
+         * Ruller med terningen. Opdaterer værdien, og returnerer den.
+         */
         public int Roll()
         {
             Value = randomNumberGenerator.GetRandomNumber(1, 6);
@@ -17,6 +21,9 @@
         }
 
 		// Constructor: Hvad skal der ske, når en ny terning produceres?
+        /**
+         * Som standard rulles terningen, når den skabes første gang.
+         */
         public Die()
         {
             Roll();
