cd216d08fecb9fcb57b37d1f07ae495fe1e101d8
esbenheick <esbenheick@gmail.com>
2014-03-25 23:24:10 +0100

    oprettelse af et objekt kaldet dieposition1
    win!

--- FormationGame/Controllers/EsbentestController.cs
+++ FormationGame/Controllers/EsbentestController.cs
@@ -14,7 +14,11 @@ namespace FormationGame.Controllers
             var Dieposition1 = new Tools.Die();
             var Dieposition2 = new Tools.Die();
 
-            return ObjectToHtml(new { die1 = Dieposition1.Value, die2 = Dieposition2.Value });
+            AddToView(Dieposition1);
+            AddToView(Dieposition2);
+            
+            return ShowAddedObjects(); 
+        
         }    
         //eksperimentet er her at få returneret to terningers Value på Dieposition 1 og 1- Det virker ikke, det lorrrt.
 
