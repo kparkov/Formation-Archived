d99823764b1271aa22a404560aefe36c6ed6e05a
esbenheick <esbenheick@gmail.com>
2014-03-25 22:59:14 +0100

    Esbens Controller update med Dieposition1-2

--- FormationGame/Controllers/EsbentestController.cs
+++ FormationGame/Controllers/EsbentestController.cs
@@ -6,10 +6,18 @@ using System.Web.Mvc;
 
 namespace FormationGame.Controllers
 {
-    public class EsbentestController : Controller
+    public class EsbentestController : FormationBaseController
     {
         public ActionResult MyNewAction(){
-            return View();
-        }        
+         
+
+            var Dieposition1 = new Tools.Die();
+            var Dieposition2 = new Tools.Die();
+
+            return ObjectToHtml(new { die1 = Dieposition1.Value, die2 = Dieposition2.Value });
+        }    
+        //eksperimentet er her at få returneret to terningers Value på Dieposition 1 og 1- Det virker ikke, det lorrrt.
+
+       
     }
 }
