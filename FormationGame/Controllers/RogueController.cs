1c5caa93037a8f3a9fd92a49f8bda76996113352
Karikara <kari.tveitaskog@gmail.com>
2014-03-31 22:35:37 +0200

    RougeAssassin

--- FormationGame/Controllers/RogueController.cs
+++ FormationGame/Controllers/RogueController.cs
@@ -10,10 +10,29 @@ namespace FormationGame.Controllers
 	public class RogueController : FormationBaseController
 	{
 
-		public ActionResult SejeKari()
-		{
-			return View();
-		}
+        public ActionResult RogueView()
+        {
+            var sneakRogue = new Rogue
+            {
+                Name = "Venomous Vera",
+                stealth = 7,
+                poisonStrength = 6,
+                charm = 2,
+                weapon = new RogueAttack
+                {
+                    Type = "dagger",
+                    IsPoisenous = true,
+                    SpecialAbilities = new List<string>
+                    {
+                        "one-handed",
+                        "dagger of death"
+                    }
+                }
+            };
+
+            return ShowObjects(sneakRogue);
+        }
+            
 
 		// KODE TIL LEKTION 3
 		public ActionResult Index()
@@ -22,12 +41,26 @@ namespace FormationGame.Controllers
 
 
 			// Denne metode kan benyttes til at vise indholdet af variabler
-			return ShowObjects(/*Slet denne kommentar og erstat med variabler*/);
+			return ShowObjects();
 		}
 	}
 
 	// MODEL:
 	public class Rogue
 	{
+        public string Name { get; set; }
+        public int stealth { get; set; }
+        public int poisonStrength { get; set; }
+        public int charm { get; set; }
+
+        public RogueAttack weapon { get; set; }
+
 	}
+    public class RogueAttack 
+    {
+        public string Type { get; set; }
+        public bool IsPoisenous { get; set; }
+        public List<string> SpecialAbilities { get; set; }
+    }
 }
+
