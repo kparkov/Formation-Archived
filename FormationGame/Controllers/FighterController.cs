2cfbd5cf04eb354555e61a1ff3dcb62849464394
esbenheick <esbenheick@gmail.com>
2014-03-31 22:35:02 +0200

    FighterController
    Added Fredrik!

--- FormationGame/Controllers/FighterController.cs
+++ FormationGame/Controllers/FighterController.cs
@@ -6,10 +6,9 @@ using System.Web.Mvc;
 
 namespace FormationGame.Controllers
 {
-	//Esbens klasser
     public class FighterController : FormationBaseController
     {
-        public ActionResult MyNewAction()
+        public ActionResult ReturnFighter()
 		{
 
             var Dieposition1 = new Tools.Die();
@@ -26,10 +25,29 @@ namespace FormationGame.Controllers
 		public ActionResult Index()
 		{
 			// Her begynder vi ved tredje lektion:
+            var swordsman = new Fighter
+            {
+                name = "Fredrik",
+                race = "menneske",
+                specialties = new List<weapontype>
+                {
+                    new weapontype {
+                        weaponclass = "swords"
+                    },
+                    new weapontype {
+                        weaponclass = "axes"
+                    }
+                },
+                minimumStrenghtRequired = 10,
+                maximumIntelligenceAllowed = 2,
+                shoulderwidth = 100,
+                IsAngry = true
 
+            };
+            
 
 			// Denne metode kan benyttes til at vise indholdet af variabler
-			return ShowObjects(/*Slet denne kommentar og erstat med variabler*/);
+			return ShowObjects(swordsman);
 		}
 
     }
@@ -37,5 +55,22 @@ namespace FormationGame.Controllers
 	// MODEL:
 	public class Fighter
 	{
+        public string name { get; set; }
+        public string race { get; set; }
+        public List <weapontype> specialties { get; set; }
+        public int minimumStrenghtRequired { get; set; }
+        public int maximumIntelligenceAllowed { get; set; }
+        public int shoulderwidth { get; set; }
+        public bool IsAngry {get; set;}
+        public string Title
+		{
+			get { return name + " The Master of " + String.Join(" and ", specialties.Select(x => x.weaponclass)); }
+        }
 	}
+
+    public class weapontype
+    {
+        public string weaponclass { get; set; }
+    }
+ 
 }
